5b76640278729546ef8ed3324765498fc6b2a8e4
Efe Beydogan <efebeydogan@hotmail.com>
2023-05-29 00:52:33 +0300

    q2

--- Project4/pvm.c
+++ Project4/pvm.c
@@ -1,13 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <fcntl.h>
+#include <unistd.h>
 
 #define BUFFER_SIZE 1024
-
-void process_frameinfo(int pfn) {
-    // Function implementation for -frameinfo option
-    printf("Processing -frameinfo: %d\n", pfn);
-}
 
 // Function to read the contents of a file
 unsigned long read_file(const char* filename, unsigned long offset) {
@@ -17,85 +14,102 @@ unsigned long read_file(const char* filename, unsigned long offset) {
         return 0;
     }
 
-    // Move the file pointer to the desired offset
-    if (lseek(fd, offset, SEEK_SET) == -1) {
-        perror("Error seeking file");
+    offset *= sizeof(unsigned long);
+
+    off_t result = lseek(fd, offset, SEEK_SET);
+
+    if (result == -1) {
         close(fd);
-        return 0;
+        perror("Error seeking file");
+        return 1;
     }
+    // // Move the file pointer to the desired offset
+    // if (lseek(fd, offset, SEEK_SET) == -1) {
+    //     perror("Error seeking file");
+    //     close(fd);
+    //     return 0;
+    // }
+
 
     unsigned long value = 0;
-    if (read(fd, &value, sizeof(unsigned long)) == -1) {
+    ssize_t bytesRead = read(fd, &value, sizeof(unsigned long));
+    if (bytesRead == -1) {
         perror("Error reading file");
         close(fd);
-        return 0;
+        return 1;
     }
 
     close(fd);
     return value;
 }
 
-// Function to find the total virtual memory size from /proc/PID/maps
-unsigned long get_virtual_memory_size(int pid) {
-    char filename[128];
-    snprintf(filename, sizeof(filename), "/proc/%d/maps", pid);
-
-    char* mapsContent = read_file(filename);
-    if (mapsContent == NULL)
-        return 0;
-
-    unsigned long totalSize = 0;
-
-    // Iterate over the lines in the maps file
-    char* line = strtok(mapsContent, "\n");
-    while (line != NULL) {
-        // Parse the line and extract the start and end addresses
-        unsigned long start, end;
-        sscanf(line, "%lx-%lx", &start, &end);
-
-        // Calculate the size of the memory region
-        unsigned long size = end - start;
-
-        // Add the size to the total
-        totalSize += size;
-
-        // Move to the next line
-        line = strtok(NULL, "\n");
-    }
-
-    free(mapsContent);
-    return totalSize / 1024; // Convert to KB
+// Function implementation for -frameinfo option
+void process_frameinfo(unsigned long pfn) {
+    unsigned long pageFlags = read_file("/proc/kpageflags", pfn);
+    unsigned long pageCounts = read_file("/proc/kpagecount", pfn);
+    printf("%lu\n%lu\n", pageFlags, pageCounts);
 }
 
-// Function to find the physical memory sizes from /proc/kpagecount
-void get_physical_memory_sizes(int pid, unsigned long* exclusiveSize, unsigned long* inclusiveSize) {
-    char filename[128];
-    snprintf(filename, sizeof(filename), "/proc/%d/kpagecount", pid);
+// // Function to find the total virtual memory size from /proc/PID/maps
+// unsigned long get_virtual_memory_size(int pid) {
+//     char filename[128];
+//     snprintf(filename, sizeof(filename), "/proc/%d/maps", pid);
 
-    // Calculate offsets based on the size of unsigned long (8 bytes)
-    unsigned long exclusiveOffset = pid * 8;
-    unsigned long inclusiveOffset = pid * 8 + 8;
+//     unsigned long mapsContent = read_file(filename, 0);
+//     if (mapsContent == NULL)
+//         return 0;
 
-    *exclusiveSize = read_file(filename, exclusiveOffset) * 4; // Each page is 4KB
-    *inclusiveSize = read_file(filename, inclusiveOffset) * 4; // Each page is 4KB
-}
+//     unsigned long totalSize = 0
 
-void process_memused(int pid) {
-    // Calculate virtual memory size
-    unsigned long virtualMemorySize = get_virtual_memory_size(pid);
-    // Calculate physical memory sizes
-    unsigned long exclusiveMemorySize, inclusiveMemorySize;
-    get_physical_memory_sizes(pid, &exclusiveMemorySize, &inclusiveMemorySize);
-    // Print the results
-    printf("Virtual Memory Size (KB): %lu\n", virtualMemorySize);
-    printf("Physical Memory (Exclusive) Size (KB): %lu\n", exclusiveMemorySize);
-    printf("Physical Memory (Inclusive) Size (KB): %lu\n", inclusiveMemorySize);
-    // Calculate physical memory sizes
-    unsigned long exclusiveMemorySize, inclusiveMemorySize;
-    get_physical_memory_sizes(pid, &exclusiveMemorySize, &inclusiveMemorySize);
-    // Function implementation for -memused option
-    printf("Processing -memused: %d\n", pid);
-}
+//     // Iterate over the lines in the maps file
+//     char* line = strtok(mapsContent, "\n");
+//     while (line != NULL) {
+//         // Parse the line and extract the start and end addresses
+//         unsigned long start, end;
+//         sscanf(line, "%lx-%lx", &start, &end);
+
+//         // Calculate the size of the memory region
+//         unsigned long size = end - start;
+
+//         // Add the size to the total
+//         totalSize += size;
+
+//         // Move to the next line
+//         line = strtok(NULL, "\n");
+//     }
+
+//     free(mapsContent);
+//     return totalSize / 1024; // Convert to KB
+// }
+
+// // Function to find the physical memory sizes from /proc/kpagecount
+// void get_physical_memory_sizes(int pid, unsigned long* exclusiveSize, unsigned long* inclusiveSize) {
+//     char filename[128];
+//     snprintf(filename, sizeof(filename), "/proc/%d/kpagecount", pid);
+
+//     // Calculate offsets based on the size of unsigned long (8 bytes)
+//     unsigned long exclusiveOffset = pid * 8;
+//     unsigned long inclusiveOffset = pid * 8 + 8;
+
+//     *exclusiveSize = read_file(filename, exclusiveOffset) * 4; // Each page is 4KB
+//     *inclusiveSize = read_file(filename, inclusiveOffset) * 4; // Each page is 4KB
+// }
+
+// void process_memused(int pid) {
+//     // Calculate virtual memory size
+//     unsigned long virtualMemorySize = get_virtual_memory_size(pid);
+//     // Calculate physical memory sizes
+//     unsigned long exclusiveMemorySize, inclusiveMemorySize;
+//     get_physical_memory_sizes(pid, &exclusiveMemorySize, &inclusiveMemorySize);
+//     // Print the results
+//     printf("Virtual Memory Size (KB): %lu\n", virtualMemorySize);
+//     printf("Physical Memory (Exclusive) Size (KB): %lu\n", exclusiveMemorySize);
+//     printf("Physical Memory (Inclusive) Size (KB): %lu\n", inclusiveMemorySize);
+//     // Calculate physical memory sizes
+//     get_physical_memory_sizes(pid, &exclusiveMemorySize, &inclusiveMemorySize);
+//     // Function implementation for -memused option
+//     printf("Processing -memused: %d\n", pid);
+// }
 
 void process_mapva(int pid, const char* va) {
     // Function implementation for -mapva option
@@ -128,75 +142,79 @@ void process_alltablesize(int pid) {
 }
 
 int main(int argc, char* argv[]) {
-    if (argc < 3) {
-        printf("Invalid number of arguments\n");
-        return 1;
-    }
+    unsigned long pfn = strtoul(argv[2], NULL, 0);
+    printf("pfn: %lu\n", pfn);
+    process_frameinfo(pfn);
+    // if (argc < 3) {
+    //     printf("Invalid number of arguments\n");
+    //     return 1;
+    // }
 
-    if (strcmp(argv[1], "-frameinfo") == 0) {
-        if (argc < 4) {
-            printf("Invalid number of arguments for -frameinfo\n");
-            return 1;
-        }
-        int pfn = atoi(argv[3]);
-        process_frameinfo(pfn);
-    } else if (strcmp(argv[1], "-memused") == 0) {
-        if (argc < 4) {
-            printf("Invalid number of arguments for -memused\n");
-            return 1;
-        }
-        int pid = atoi(argv[3]);
-        process_memused(pid);
-    } else if (strcmp(argv[1], "-mapva") == 0) {
-        if (argc < 5) {
-            printf("Invalid number of arguments for -mapva\n");
-            return 1;
-        }
-        int pid = atoi(argv[3]);
-        const char* va = argv[4];
-        process_mapva(pid, va);
-    } else if (strcmp(argv[1], "-pte") == 0) {
-        if (argc < 5) {
-            printf("Invalid number of arguments for -pte\n");
-            return 1;
-        }
-        int pid = atoi(argv[3]);
-        const char* va = argv[4];
-        process_pte(pid, va);
-    } else if (strcmp(argv[1], "-maprange") == 0) {
-        if (argc < 6) {
-            printf("Invalid number of arguments for -maprange\n");
-            return 1;
-        }
-        int pid = atoi(argv[3]);
-        const char* va1 = argv[4];
-        const char* va2 = argv[5];
-        process_maprange(pid, va1, va2);
-    } else if (strcmp(argv[1], "-mapall") == 0) {
-        if (argc < 4) {
-            printf("Invalid number of arguments for -mapall\n");
-            return 1;
-        }
-        int pid = atoi(argv[3]);
-        process_mapall(pid);
-    } else if (strcmp(argv[1], "-mapallin") == 0) {
-        if (argc < 4) {
-            printf("Invalid number of arguments for -mapallin\n");
-            return 1;
-        }
-        int pid = atoi(argv[3]);
-        process_mapallin(pid);
-    } else if (strcmp(argv[1], "-alltablesize") == 0) {
-        if (argc < 4) {
-            printf("Invalid number of arguments for -alltablesize\n");
-            return 1;
-        }
-        int pid = atoi(argv[3]);
-        process_alltablesize(pid);
-    } else {
-        printf("Invalid option: %s\n", argv[1]);
-        return 1;
-    }
+    // if (strcmp(argv[1], "-frameinfo") == 0) {
+    //     if (argc != 3) {
+    //         printf("Invalid number of arguments for -frameinfo\n");
+    //         return 1;
+    //     }
+
+    //     unsigned long pfn = strtoul(argv[2], NULL, 0);
+    //     process_frameinfo(pfn);
+    // } else if (strcmp(argv[1], "-memused") == 0) {
+    //     if (argc < 4) {
+    //         printf("Invalid number of arguments for -memused\n");
+    //         return 1;
+    //     }
+    //     int pid = atoi(argv[3]);
+    //     process_memused(pid);
+    // } else if (strcmp(argv[1], "-mapva") == 0) {
+    //     if (argc < 5) {
+    //         printf("Invalid number of arguments for -mapva\n");
+    //         return 1;
+    //     }
+    //     int pid = atoi(argv[3]);
+    //     const char* va = argv[4];
+    //     process_mapva(pid, va);
+    // } else if (strcmp(argv[1], "-pte") == 0) {
+    //     if (argc < 5) {
+    //         printf("Invalid number of arguments for -pte\n");
+    //         return 1;
+    //     }
+    //     int pid = atoi(argv[3]);
+    //     const char* va = argv[4];
+    //     process_pte(pid, va);
+    // } else if (strcmp(argv[1], "-maprange") == 0) {
+    //     if (argc < 6) {
+    //         printf("Invalid number of arguments for -maprange\n");
+    //         return 1;
+    //     }
+    //     int pid = atoi(argv[3]);
+    //     const char* va1 = argv[4];
+    //     const char* va2 = argv[5];
+    //     process_maprange(pid, va1, va2);
+    // } else if (strcmp(argv[1], "-mapall") == 0) {
+    //     if (argc < 4) {
+    //         printf("Invalid number of arguments for -mapall\n");
+    //         return 1;
+    //     }
+    //     int pid = atoi(argv[3]);
+    //     process_mapall(pid);
+    // } else if (strcmp(argv[1], "-mapallin") == 0) {
+    //     if (argc < 4) {
+    //         printf("Invalid number of arguments for -mapallin\n");
+    //         return 1;
+    //     }
+    //     int pid = atoi(argv[3]);
+    //     process_mapallin(pid);
+    // } else if (strcmp(argv[1], "-alltablesize") == 0) {
+    //     if (argc < 4) {
+    //         printf("Invalid number of arguments for -alltablesize\n");
+    //         return 1;
+    //     }
+    //     int pid = atoi(argv[3]);
+    //     process_alltablesize(pid);
+    // } else {
+    //     printf("Invalid option: %s\n", argv[1]);
+    //     return 1;
+    // }
 
     return 0;
 }
